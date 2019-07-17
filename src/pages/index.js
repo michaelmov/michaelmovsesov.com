9eb734f47b36d270c6c1907fb0de87eaf7731dfd
Michael Movsesov <michael.movsesov@gmail.com>
2019-07-16 20:47:15 -0700

    Fixed casing issue

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 
-import Layout from '../components/layout';
+import Layout from '../components/Layout';
 import HomeHeader from '../components/HomeHeader';
 import ArticleCard from '../components/ArticleCard';
 import SEO from '../components/seo';
