38ee64c540ec18022d66b3d80bb7602d372bd19e
Michael Movsesov <michael.movsesov@gmail.com>
2019-07-21 09:52:11 -0700

    Added article header title paallax effect on scroll

--- src/templates/Article.js
+++ src/templates/Article.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import { useEffect } from 'react';
 import { graphql } from 'gatsby';
-import Img from 'gatsby-image';
+import BackgroundImage from 'gatsby-background-image';
 import { DiscussionEmbed } from 'disqus-react';
 import avatarImage from './../../content/assets/img/michael_movsesov_avatar_small.jpg';
 
@@ -18,22 +19,52 @@ const Article = ({ data }) => {
     title: article.frontmatter.title,
   };
 
+  const initTitleParallax = () => {
+    const articleHeroSection = document.querySelector('.article__hero');
+    const articleHeroText = document.querySelector('.article__hero-text ');
+    const heroHeight = articleHeroSection.clientHeight;
+
+    // Header parallax
+    window.addEventListener('scroll', e => {
+      const scroll = window.scrollY;
+
+      if (scroll <= heroHeight) {
+        articleHeroText.style.transform = `translate(0px, ${scroll / 3.5}%)`;
+        articleHeroText.style.opacity = 1 - scroll / 250;
+      }
+    });
+
+    // Make all article images 100% width
+    const articleImages = document.querySelectorAll('#article-wrapper img');
+    articleImages.forEach(image => {
+      if (image.parentElement.nodeName === 'P') {
+        image.parentElement.style.width = '100%';
+      }
+    });
+  };
+
+  useEffect(() => {
+    initTitleParallax();
+  }, []);
+
   if (article.frontmatter.hero_image) {
     heroImage = (
-      <Img
+      <BackgroundImage
         className="article__hero-image"
         fluid={article.frontmatter.hero_image.childImageSharp.fluid}
-        objectFit="cover"
-        objectPosition="50% 50%"
       />
     );
   }
   return (
     <Layout>
       <SEO title={article.frontmatter.title} />
-      <header className="article__hero d-flex align-items-center justify-content-center text-white">
+      <header
+        className={`article__hero d-flex align-items-center justify-content-center text-white ${
+          heroImage ? 'article__hero-bg-overlay' : ''
+        }`}
+      >
         {heroImage}
-        <div className="container-fluid">
+        <div className="container-fluid" style={{ zIndex: 1 }}>
           <div className="row">
             <div className="col-12 article__hero-text">
               <h1 className="h2 text-center article__hero-title mb-4">
