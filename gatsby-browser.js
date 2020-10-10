5e6ef3684fb1d6938bee02295093eb1e6f0147fe
Michael Movsesov <michael.movsesov@gmail.com>
2020-10-09 22:27:55 -0700

    npm package updates

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -7,7 +7,7 @@
 export const onClientEntry = () => {
   // IntersectionObserver polyfill for gatsby-background-image (Safari, IE)
   if (typeof window.IntersectionObserver === `undefined`) {
-    import(`intersection-observer`);
+    const interesectionObserver = import(`intersection-observer`);
     console.log(`# IntersectionObserver is polyfilled!`);
   }
 };
